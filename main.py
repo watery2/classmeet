e6adf0067a72ad4587be7afe52e9d351c7bad3c8
watery2 <68787282+watery2@users.noreply.github.com>
2021-01-12 10:22:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def go(link):
         "profile.default_content_setting_values.geolocation": 1,
         "profile.default_content_setting_values.notifications": 1
     })
-    path = r"C:\Users\otoma\Desktop\python project\chromedriver\chromedriver.exe"
+    path = r"C:\Users\guy\Desktop\python project\chromedriver\chromedriver.exe"
     browser = webdriver.Chrome(options=opt, executable_path=path)
     browser.get("https://gmail.com")
     browser.find_element_by_xpath('//*[@id="identifierId"]').send_keys("****@gmail.com")
